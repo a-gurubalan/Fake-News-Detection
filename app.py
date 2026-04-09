61d837feeabfc54f174b72efa0552dba27cb3bf6
a-gurubalan <gurubalang51@gmail.com>
2026-04-09 19:46:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,22 @@ except Exception as e:
 
 news_input = st.text_area("News Article:")
 
-if st.button("Check News"):
+if st.button("check News"):
     if news_input.strip():
         transform_input = vectorizer.transform([news_input])
         prediction = model.predict(transform_input)
 
+        proba = model.predict_proba(transform_input)
+        real_prob = proba[0][1] * 100
+        fake_prob = proba[0][0] * 100
+
         if prediction[0] == 1:
-            st.success("Real News ✅")
+            st.success("The News is Real! ✅")
         else:
-            st.error("Fake News ❌")
+            st.error("The News is Fake! ❌")
+
+        st.write(f"Real: {real_prob:.2f}%")
+        st.write(f"Fake: {fake_prob:.2f}%")
+
     else:
-        st.warning("Enter text")
+        st.warning("Please enter some text")
